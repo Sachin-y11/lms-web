5bde6a3f0c133a47765f002a0945b23f742d517a
Sachin <sachinyadav19812@gmail.com>
2026-08-25 20:15:55 +0530

    Implement drag-and-drop reordering for chapters and lessons with server-side updates

--- app/admin/courses/[courseId]/edit/_components/CourseStructure.tsx
+++ app/admin/courses/[courseId]/edit/_components/CourseStructure.tsx
@@ -36,6 +36,8 @@ import {
 } from "lucide-react"
 import Link from "next/link"
 import { useState } from "react"
+import { toast } from "sonner"
+import { reorderChapter, reorderLessons } from "../actions"
 
 interface sotableItemProps {
   id: string
@@ -100,15 +102,157 @@ export default function CourseStructure({
   const handleDragEvent = (event: DragEndEvent) => {
     const { active, over } = event
 
-    if (active.id !== over?.id) {
-      setItems((items) => {
-        const oldIndex = items.findIndex(
-          (item) => item.id === String(active.id)
-        )
-        const newIndex = items.findIndex((item) => item.id === String(over?.id))
+    if (!over || active.id === over.id) {
+      return
+    }
 
-        return arrayMove(items, oldIndex, newIndex)
+    const activeId = active.id
+    const overId = over.id
+    const activeType = active.data.current?.type as "chapter" | "lesson"
+    const overType = over.data.current?.type as "chapter" | "lesson"
+
+    const courseId = data.id
+
+    if (activeType === "chapter") {
+      let targetChapterId = null
+
+      if (overType === "chapter") {
+        targetChapterId = overId
+      } else if (overType === "lesson") {
+        targetChapterId = over.data.current?.chapterId ?? null
+      }
+
+      if (!targetChapterId) {
+        toast.error("Invalid target chapter")
+        return
+      }
+
+      const oldIndex = items.findIndex((item) => item.id === activeId)
+      const newIndex = items.findIndex((item) => item.id === targetChapterId)
+
+      if (oldIndex === -1 || newIndex === -1) {
+        toast.error("Invalid index")
+        return
+      }
+
+      const reorderLocalChapter = arrayMove(items, oldIndex, newIndex)
+
+      const updatedChapterForState = reorderLocalChapter.map((item, index) => {
+        return {
+          ...item,
+          order: index + 1,
+        }
       })
+
+      const previousItem = [...items]
+
+      setItems(updatedChapterForState)
+
+      if (courseId) {
+        const chaptersToUpdate = updatedChapterForState.map((chapter) => ({
+          id: chapter.id,
+          position: chapter.order,
+        }))
+
+        const reorderChapterPromise = () =>
+          reorderChapter({
+            chapters: chaptersToUpdate,
+            courseId,
+          })
+
+        toast.promise(reorderChapterPromise(), {
+          loading: "Reordering chapter...",
+          success: (result) => {
+            if (result.status === "success") return result.message
+            throw new Error(result.message)
+          },
+          error: () => {
+            setItems(previousItem)
+            return "Failed to reorder"
+          },
+        })
+      }
+    }
+
+    if (activeType === "lesson" && overType === "lesson") {
+      const chapterId = active.data.current?.chapterId
+      const overChapterId = over.data.current?.chapterId
+      if (!chapterId || !overChapterId) {
+        toast.error("Invalid target chapter")
+        return
+      }
+
+      const chapterIndex = items.findIndex((item) => item.id === chapterId)
+
+      if (chapterIndex === -1) {
+        toast.error("Invalid index")
+        return
+      }
+
+      const chapterToUpdate = items[chapterIndex]
+
+      const oldLessonIndex = chapterToUpdate.lessons.findIndex(
+        (lesson) => lesson.id === activeId
+      )
+      const newLessonIndex = chapterToUpdate.lessons.findIndex(
+        (lesson) => lesson.id === overId
+      )
+
+      if (oldLessonIndex === -1 || newLessonIndex === -1) {
+        toast.error("Invalid index")
+        return
+      }
+
+      const reorderLocalLesson = arrayMove(
+        chapterToUpdate.lessons,
+        oldLessonIndex,
+        newLessonIndex
+      )
+
+      const updatedLessonForState = reorderLocalLesson.map((item, index) => {
+        return {
+          ...item,
+          order: index + 1,
+        }
+      })
+
+      const newItems = [...items]
+
+      newItems[chapterIndex] = {
+        ...chapterToUpdate,
+        lessons: updatedLessonForState,
+      }
+
+      const previousItem = [...items]
+
+      setItems(newItems)
+
+      if (courseId) {
+        const lessonToUpdate = updatedLessonForState.map((lesson) => ({
+          id: lesson.id,
+          position: lesson.order,
+        }))
+
+        const reorderLessonsPromise = reorderLessons({
+          chapterId: chapterId,
+          lessons: lessonToUpdate,
+          courseId: courseId,
+        })
+
+        toast.promise(reorderLessonsPromise, {
+          loading: "Reordering lessons...",
+          success: (result) => {
+            if (result.status === "success") return result.message
+            throw new Error(result.message)
+          },
+          error: () => {
+            setItems(previousItem)
+            return "Failed to reorder"
+          },
+        })
+      }
+
+      return
     }
   }
 
@@ -137,7 +281,7 @@ export default function CourseStructure({
         <CardHeader className="flow-row flex items-center justify-between border-b border-border">
           <CardTitle>Chapters</CardTitle>
         </CardHeader>
-        <CardContent>
+        <CardContent className="space-y-8">
           <SortableContext items={items} strategy={verticalListSortingStrategy}>
             {items.map((item) => (
               <SortableItem
@@ -196,7 +340,7 @@ export default function CourseStructure({
                             {item.lessons.map((lesson) => (
                               <SortableItem
                                 key={lesson.id}
-                                id={item.id}
+                                id={lesson.id}
                                 data={{ type: "lesson", chapterId: item.id }}
                               >
                                 {(lessonsListeners) => (
--- app/admin/courses/[courseId]/edit/actions.ts
+++ app/admin/courses/[courseId]/edit/actions.ts
@@ -4,6 +4,7 @@ import { requireAdmin } from "@/app/data/admin/require-admin"
 import prisma from "@/lib/prisma"
 import { ApiResponse } from "@/lib/type"
 import { courseSchema, CourseSchemaType } from "@/lib/zod-schema"
+import { revalidatePath } from "next/cache"
 
 export async function editCourse(
   data: CourseSchemaType,
@@ -30,6 +31,97 @@ export async function editCourse(
     return {
       status: "success",
       message: "Course updated successfully",
+    }
+  } catch (error) {
+    return {
+      status: "error",
+      message: `${error}`,
+    }
+  }
+}
+
+export async function reorderLessons({
+  chapterId,
+  lessons,
+  courseId,
+}: {
+  chapterId: string
+  lessons: { id: string; position: number }[]
+  courseId: string
+}): Promise<ApiResponse> {
+  await requireAdmin()
+
+  try {
+    if (!lessons || lessons.length === 0) {
+      return {
+        status: "error",
+        message: "No lessons found",
+      }
+    }
+
+    const updates = lessons.map((lesson) =>
+      prisma.lesson.update({
+        where: {
+          id: lesson.id,
+          chapterId: chapterId,
+        },
+        data: {
+          position: lesson.position,
+        },
+      })
+    )
+
+    await prisma.$transaction(updates)
+
+    revalidatePath(`/admin/courses/${courseId}/edit`)
+
+    return {
+      status: "success",
+      message: "Lessons reordered successfully",
+    }
+  } catch (error) {
+    return {
+      status: "error",
+      message: `${error}`,
+    }
+  }
+}
+
+export async function reorderChapter({
+  chapters,
+  courseId,
+}: {
+  chapters: { id: string; position: number }[]
+  courseId: string
+}): Promise<ApiResponse> {
+  await requireAdmin()
+  try {
+    if (!chapters || chapters.length === 0) {
+      return {
+        status: "error",
+        message: "No chapters provided for reorderig.",
+      }
+    }
+
+    const updates = chapters.map((chapter) =>
+      prisma.chapter.update({
+        where: {
+          id: chapter.id,
+          courseId: courseId,
+        },
+        data: {
+          position: chapter.position,
+        },
+      })
+    )
+
+    await prisma.$transaction(updates)
+
+    revalidatePath(`/admin/courses/${courseId}/edit`)
+
+    return {
+      status: "success",
+      message: "Chapter reordered successfully",
     }
   } catch (error) {
     return {
